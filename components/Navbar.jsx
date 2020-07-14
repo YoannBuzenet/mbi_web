e07f847c502bef498b2f3321f772de69399d91b7
Yoann <ybuzenet@gmail.com>
2020-07-14 18:38:43 +0200

    CSS looks better

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Navbar = () => {
-  return <>NavBar</>;
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="container">
+          <div className="menu-elements">
+            <span>MTG INTERFACE</span>
+            <ul>
+              <li>Software</li>
+              <li>About Us</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 export default Navbar;
--- pages/index.js
+++ pages/index.js
@@ -15,21 +15,59 @@ export default function Home() {
       <Navbar />
 
       <main>
-        <div className="container">Let's build</div>
-        <div className="first-liner blue">
+        <div className="first-liner">
           <div className="container">
-            <div className="main-title">
-              <div className="left-content">
-                <h1 className="main-punchline">
-                  Buying cards has never been so easy
-                </h1>
+            <div className="first-liner">
+              <div className="software-image">
+                <img src="/images/software_front.png" />
               </div>
-              <div className="right-content"></div>
+              <div className="main-title">
+                <div className="left-content">
+                  <h1 className="main-punchline">
+                    Buying cards has never been so easy
+                  </h1>
+                </div>
+                <div className="right-content"></div>
+              </div>
             </div>
           </div>
         </div>
         <div className="second-liner">
-          <div className="container">Yeah</div>
+          <div className="container">
+            <div className="cards-container">
+              <div className="card">
+                <span className="card-title">Adjective 1</span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Mollitia ex modi fugit at expedita, fuga laboriosam doloribus in
+                quos optio!
+              </div>
+              <div className="card">
+                <span className="card-title">Adjective 2</span>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos
+                veniam quasi rerum. Ipsum ipsa corrupti odit natus beatae
+                perspiciatis libero?
+              </div>
+              <div className="card">
+                <span className="card-title">Adjective 3</span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Laudantium odit error voluptatum exercitationem, harum incidunt
+                facere a assumenda ipsa minus.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="third-liner">
+          <div className="container">
+            <h2 className="punchline">
+              Fix your prices. Let the customers come.
+            </h2>
+            <p>
+              MTG Interface enables you to fix all your buying prices for any
+              MTG card : regular, foil, signed, in any condition or language.
+              Your customers just have to connect, check your prices, and sell
+              you their cards.
+            </p>
+          </div>
         </div>
       </main>
 
