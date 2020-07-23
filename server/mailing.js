5fa3471b082014838b68d95648709a3130ca816f
Yoann <ybuzenet@gmail.com>
2020-07-23 20:57:17 +0200

    testing around mail sending

--- server/mailing.js
+++ server/mailing.js
@@ -12,13 +12,16 @@ async function mailLeadToAdmin(userData) {
   });
 
   // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Yoann" <ybuzenet@gmail.com>', // sender address
-    to: "ybuzenet@gmail.com", // list of receivers
-    subject: "A new person wants to be contacted about MTG Interface", // Subject line
-    text: `Hey ! ${userData.complete_name} wants to be contacted. The email is ${userData.email} and the phone number is ${userData.phone_number}. Name of the shop is ${userData.shop_name}, from the country ${userData.country}. Message box did contain the following ${userData.message}. Let's go !`, // plain text body
-    html: `<h1>Hey !</h1> <p>${userData.complete_name} wants to be contacted. </p><p>The email is ${userData.email} and the phone number is ${userData.phone_number}. </p><p>Name of the shop is ${userData.shop_name}, from the country ${userData.country}. </p><p>Message box did contain the following ${userData.message}.</p><p>Let's go !</p>`, // html body
-  });
+  let info = await transporter
+    .sendMail({
+      from: '"Yoann" <ybuzenet@gmail.com>', // sender address
+      to: "ybuzenet@gmail.com", // list of receivers
+      subject: "A new person wants to be contacted about MTG Interface", // Subject line
+      text: `Hey ! ${userData.complete_name} wants to be contacted. The email is ${userData.email} and the phone number is ${userData.phone_number}. Name of the shop is ${userData.shop_name}, from the country ${userData.country}. Message box did contain the following ${userData.message}. Let's go !`, // plain text body
+      html: `<h1>Hey !</h1> <p>${userData.complete_name} wants to be contacted. </p><p>The email is ${userData.email} and the phone number is ${userData.phone_number}. </p><p>Name of the shop is ${userData.shop_name}, from the country ${userData.country}. </p><p>Message box did contain the following ${userData.message}.</p><p>Let's go !</p>`, // html body
+    })
+    .catch((e) => console.log(e));
+  console.log("Message sent: %s", info.messageId);
 }
 
 module.exports = { mailLeadToAdmin };
