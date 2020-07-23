be2dcc7e2c4662c936e16b6420d0a14acbd3fce2
Yoann <ybuzenet@gmail.com>
2020-07-23 23:11:55 +0200

    pop up CSS improvment + onCLick debug on black div

--- components/PopUp.jsx
+++ components/PopUp.jsx
@@ -1,11 +1,10 @@
 const PopUp = ({ setIsPopUpDisplayed }) => {
   return (
-    <div className="blackDiv" onClicl={(e) => setIsPopUpDisplayed(false)}>
+    <div className="blackDiv" onClick={(e) => setIsPopUpDisplayed(false)}>
       <div className="popUpContainer">
         <div>
           <p>We received your message.</p>
           <p>We will contact you back in the next 24 hours.</p>
-          Pop Up{" "}
           <button
             onClick={(e) => setIsPopUpDisplayed(false)}
             className="CTA-button"
